3f2e66393eb6f141fb1176ffe21ed6c59ab7d772
mihmilicio <milenamilicio@hotmail.com>
2023-08-08 16:50:47 -0300

    Implement Amphibian Service in the app

--- app/src/main/java/com/example/amphibians/ui/AmphibianListFragment.kt
+++ app/src/main/java/com/example/amphibians/ui/AmphibianListFragment.kt
@@ -33,9 +33,11 @@ class AmphibianListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         val binding = FragmentAmphibianListBinding.inflate(inflater)
-        // TODO: call the view model method that calls the amphibians api
+
+        viewModel.getAmphibianList()
+
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
         binding.recyclerView.adapter = AmphibianListAdapter(AmphibianListener { amphibian ->
